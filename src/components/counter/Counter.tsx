f1657aa37568d418056348897817b811352037e7
DESKTOP-EDC8M7O\yaduo <yaduol@hotmail.com>
2016-11-11 10:53:47 -0800

    Redux involved - change in counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,26 +1,40 @@
 import * as React from 'react';
+import { createStore } from 'redux';
+
+let store = createStore(
+    (state:any, action:any) => {
+        switch (action.type) {
+            case 'INCR':
+                return { counter: state.counter + action.by };
+            default:
+                return state;
+        }
+    },
+    { counter: 0 }
+);
 
 export default class Counter extends React.Component<any, any> {
 
-    constructor(props: any, context: any) {
-        super(props, context);
-        this.state = { counter: 0 };
+    private unsubscribe: Function;
+    
+    componentDidMount() {
+        this.unsubscribe = store.subscribe(() => this.forceUpdate());
     }
-
+    
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    
     render() {
         return (
             <div>
                 <p>
-                    <label>Counter: </label><b>#{this.state.counter}</b>
+                    <label>Counter: </label><b>#{store.getState().counter}</b>
                 </p>
-                <button onClick={e => this.incr(1) }>INCREMENT</button>
+                <button onClick={e => store.dispatch({ type:'INCR', by: 1 }) }>INCREMENT</button>
                 <span style={{ padding: "0 5px" }} />
-                <button onClick={e => this.incr(-1) }>DECREMENT</button>
+                <button onClick={e => store.dispatch({ type:'INCR', by: -1 }) }>DECREMENT</button>
             </div>
         );
-    }
-
-    incr(by:number) {
-        this.setState({ counter: this.state.counter + by });
     }
 }
